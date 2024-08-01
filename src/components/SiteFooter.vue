a4f20c8d4a204209564387c55add947cf3d76958
Jeremy Nayet <71263473+Jeremy-Epitech@users.noreply.github.com>
2024-08-01 21:56:02 +0200

    useless changes since it is already done zbi

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -3,24 +3,45 @@
     <div class="container">
       <div class="footer-content">
         <div class="footer-section">
-          <h3>GreenWild</h3>
-          <p>Protégeons notre planète ensemble. Rejoignez-nous dans notre mission de conservation et de sensibilisation à l'environnement.</p>
-        </div>
-        <div class="footer-section">
           <h4>Contactez-nous</h4>
-          <p><a href="mailto:contact@greenwild.fr">contact@greenwild.fr</a></p>
-          <p>SIREN : 930 755 848</p>
+          <div>
+            <p>
+              <a href="mailto:contact@greenwild.fr">contact@greenwild.fr</a>
+            </p>
+            <p>SIREN : 930 755 848</p>
+          </div>
         </div>
+
+        <div class="footer-section">
+          <h3>GreenWild</h3>
+          <p>
+            Protégeons notre planète ensemble. Rejoignez-nous dans notre mission
+            de conservation et de sensibilisation à l'environnement.
+          </p>
+        </div>
+
         <div class="footer-section">
           <h4>Suivez-nous</h4>
           <a target="_blank" href="https://www.facebook.com/greenwild">
-            <img src="@/assets/facebook-icon.png" alt="Facebook" class="social-icon" />
+            <img
+              src="@/assets/facebook-icon.png"
+              alt="Facebook"
+              class="social-icon"
+            />
           </a>
           <a target="_blank" href="https://www.instagram.com/greenwild.france/">
-            <img src="@/assets/instagram-icon.png" alt="Instagram" class="social-icon" />
+            <img
+              src="@/assets/instagram-icon.png"
+              alt="Instagram"
+              class="social-icon"
+            />
           </a>
           <a target="_blank" href="https://www.linkedin.com/company/greenwild">
-            <img src="@/assets/linkedin-icon.png" alt="LinkedIn" class="social-icon" />
+            <img
+              src="@/assets/linkedin-icon.png"
+              alt="LinkedIn"
+              class="social-icon"
+            />
           </a>
         </div>
       </div>
@@ -34,8 +55,8 @@
 
 <script>
 export default {
-  name: 'SiteFooter',
-}
+  name: "SiteFooter",
+};
 </script>
 
 <style scoped>
